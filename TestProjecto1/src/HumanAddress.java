d88e3cd4e3399b123723785da39a4b2335d8765e
Nanasuzu0822 <nana_0822@ezweb.ne.jp>
2018-01-26 21:48:10 +0900

    0126

---
+++ TestProjecto1/src/HumanAddress.java
@@ -0,0 +1,9 @@
+
+public class HumanAddress {
+
+	public String address;
+	public String getAddress(){
+		address="東京";
+		return address;
+	}
+}
---
+++ TestProjecto1/src/HumanAge.java
@@ -0,0 +1,9 @@
+
+public class HumanAge {
+
+	public int age;
+	public int getAge(){
+		age=25;
+		return age;
+	}
+}
