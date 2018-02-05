15c47f6663c9885166e23c94cdba8503746da858
nanasuzu0822 <nana_0822@ezweb.ne.jp>
2018-02-05 21:44:48 +0900

    2/5

--- Person/src/Person.java
+++ Person/src/Person.java
@@ -1,27 +1,14 @@
 
-public class Person {
+public class Person{
 	public String name = null;
-	public int age =0;
-	
-	public Person(){}
-	
-	public Person(String name,int age){
+	public int age = 0;
+
+	public String getName(){
+	return this.name;
+
+	public void setName(String name){
 		this.name = name;
-		this.age = age;
 	}
-		public Person(String name){
-			this.name = name;
-			this.age = 0;
 		}
-		
-		public Person(int age){
-			this.name="名前なし";
-			this.age = age;
-		}
-		public Person(int age,String name){
-		this.name = name;
-		this.age = age;
-		}
-}
 
-
+	}
